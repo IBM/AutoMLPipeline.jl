25273cf72e99bf8fa4b96c6902b5fe5b8456b66b
Paulito Palmes <ppalmes@gmail.com>
2020-03-09 13:36:38 +0000

    fixes in ValDate filter

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 module TestAutoMLPipeline
 using Test
 
+# suppress warnings
+using PyCall
+warnings = pyimport("warnings")
+warnings.filterwarnings("ignore")
+
 include("test_pipeline.jl")
 include("test_skpreprocessing.jl")
 include("test_decisiontree.jl")
@@ -9,9 +14,10 @@ include("test_ensemble.jl")
 include("test_crossvalidator.jl")
 include("test_skcrossvalidator.jl")
 include("test_featureselector.jl")
+include("test_valdate.jl")
+
 #include("test_mlbase.jl")
 #include("test_tsclassifier.jl")
-#include("test_valdate.jl")
 #include("test_statifier.jl")
 #include("test_monotonicer.jl")
 #include("test_cliwrapper.jl")
