5bc94793c9e9c0a7bc85f39519c8eb42ec36422f
Paulito Palmes <ppalmes@gmail.com>
2021-03-30 22:57:26 +0100

    change PYTHON env to ""

--- deps/build.jl
+++ deps/build.jl
@@ -4,11 +4,13 @@ using Conda
 function installpypackage()
 	try
 		pyimport_conda("sklearn", "scikit-learn")
+      println("scikit-learn successfully installed")
 	catch
 		try
 			Conda.add("scikit-learn")
 			pyimport_conda("sklearn", "scikit-learn")
 			pyimport_conda("sklearn.decomposition", "scikit-learn")
+			println("scikit-learn successfully installed")
 		catch
 			println("scikit-learn failed to install")
 		end
