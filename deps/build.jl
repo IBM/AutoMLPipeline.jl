ef815d1ecffd4401a19787c8375ae3f012acc12d
Paulito Palmes <ppalmes@gmail.com>
2021-03-31 01:07:07 +0100

    add mkl in deps

--- deps/build.jl
+++ deps/build.jl
@@ -8,9 +8,11 @@ function installpypackage()
       println("mkl and scikit-learn successfully installed")
 	catch
 		try
+			Conda.add("mkl")
 			Conda.add("scikit-learn")
 			pyimport_conda("sklearn", "scikit-learn")
 			pyimport_conda("sklearn.decomposition", "scikit-learn")
+         pyimport_conda("mkl", "mkl")
 			println("mkl and scikit-learn successfully installed")
 		catch
 			println("mkl and scikit-learn failed to install")
