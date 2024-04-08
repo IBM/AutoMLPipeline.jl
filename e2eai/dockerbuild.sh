ae70b1f8c90ca53a5f0593a11029b155c8d785c9
ppalmes <ppalmes@gmail.com>
2024-04-08 21:42:06 +0100

    push to docker hub
    Signed-off-by: ppalmes <ppalmes@gmail.com>

--- e2eai/dockerbuild.sh
+++ e2eai/dockerbuild.sh
@@ -1,5 +1,6 @@
 nerdctl build -t automlpipeline:latest .
 nerdctl tag automlpipeline:latest ppalmes/automlpipeline:latest
+nerdctl run -it --rm automlpipeline:latest julia --project main.jl 5 1
 #sudo docker login
 #sudo docker push ppalmes/automlpipeline:latest
 
