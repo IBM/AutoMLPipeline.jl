286f069353a145dfefa06f8ab8dce67bcd6e9221
ppalmes <ppalmes@gmail.com>
2024-04-08 22:00:31 +0100

    push to docker registry
    Signed-off-by: ppalmes <ppalmes@gmail.com>

--- e2eai/dockerbuild.sh
+++ e2eai/dockerbuild.sh
@@ -1,6 +1,6 @@
 nerdctl build -t automlpipeline:latest .
 nerdctl tag automlpipeline:latest ppalmes/automlpipeline:latest
 nerdctl run -it --rm automlpipeline:latest julia --project main.jl 5 1
-#sudo docker login
-#sudo docker push ppalmes/automlpipeline:latest
+nerdctl login registry.docker.com
+nerdctl push ppalmes/automlpipeline:latest
 
