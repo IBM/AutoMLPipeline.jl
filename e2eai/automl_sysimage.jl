0370b40f45ce5e6224f258e6ee49a384742e0864
ppalmes <ppalmes@gmail.com>
2024-05-01 22:50:37 +0100

    low/high csv input/output regression/classification
    Signed-off-by: ppalmes <ppalmes@gmail.com>

--- e2eai/automl_sysimage.jl
+++ e2eai/automl_sysimage.jl
@@ -1,6 +1,6 @@
 using PackageCompiler
 create_sysimage(["AMLPipelineBase","DataFrames","StatsBase",
-                  "CSV","Dates","Distributed",
-                  "Random","ArgParse","Test",
-                  "Statistics","Serialization"], 
+                  "ArgParse","AutoMLPipeline","CSV","Dates","Distributed",
+                  "Random","ArgParse","Test","Distributed","PythonCall",
+                  "Statistics","Serialization","StatsBase","Test"], 
                   sysimage_path="automl.so", precompile_execution_file="automl_precompile.jl")
