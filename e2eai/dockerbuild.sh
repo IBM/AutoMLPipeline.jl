08ae3e85c3e45865ac307f7c8d551f5881545832
ppalmes <ppalmes@gmail.com>
2024-05-12 22:44:30 +0100

    argo argument passing with argparse
    Signed-off-by: ppalmes <ppalmes@gmail.com>

--- e2eai/dockerbuild.sh
+++ e2eai/dockerbuild.sh
@@ -5,6 +5,9 @@ sudo docker tag automlpipeline:latest ppalmes/automlpipeline:latest
 sudo docker login registry.docker.com
 sudo docker push ppalmes/automlpipeline:latest
 
+sudo docker run --rm -v $PWD:/data  ppalmes/automlpipeline:latest -w 5 -o /data/output.txt iris.csv
+julia --project main_parse.jl -c "high" -t "classification"  ../data/diabetes.csv
+
 sudo docker build --no-cache -t amlp:latest -f ./Dockerfile.sysimage .
 sudo docker tag amlp:latest ppalmes/amlp:latest
 #sudo docker run -it --rm ppalmes/amlp:latest julia --project main.jl 5 1
