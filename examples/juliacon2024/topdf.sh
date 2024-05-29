80ffcc15324721a2add90a3f81785512b62a54ec
ppalmes <ppalmes@gmail.com>
2024-05-29 17:17:04 +0100

    more examples and images
    Signed-off-by: ppalmes <ppalmes@gmail.com>

---
+++ examples/juliacon2024/topdf.sh
@@ -0,0 +1,4 @@
+# https://rise.readthedocs.io/en/5.7.0/exportpdf.html 
+1. ~/.julia/conda/3/aarch64/bin/jupyter  nbconvert --to slides e2eai.ipynb --post serve       
+2. Add ?print-pdf to the query string as http://127.0.0.1:8000/your_talk.slides.html?print-pdf
+3. Ctrl+P to print and save to pdf
