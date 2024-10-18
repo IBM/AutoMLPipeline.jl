1e6d5225d46ec4d72f128c940b5ce50c2759e45c
Paulito Palmes, PhD <ppalmes@gmail.com>
2024-10-18 15:30:43 +0100

    reformat based on lsp

--- src/skcrossvalidator.jl
+++ src/skcrossvalidator.jl
@@ -1,7 +1,7 @@
 module SKCrossValidators
 
 import PythonCall
-const PYC=PythonCall
+const PYC = PythonCall
 
 # standard included modules
 using DataFrames
@@ -20,49 +20,49 @@ const metric_dict = Dict{String,PYC.Py}()
 const SKM = PYC.pynew()
 
 function __init__()
-   PYC.pycopy!(SKM, PYC.pyimport("sklearn.metrics"))
+    PYC.pycopy!(SKM, PYC.pyimport("sklearn.metrics"))
 
-   metric_dict["roc_auc_score"]                   = SKM.roc_auc_score
-   metric_dict["accuracy_score"]                  = SKM.accuracy_score
-   metric_dict["auc"]                             = SKM.auc
-   metric_dict["average_precision_score"]         = SKM.average_precision_score
-   metric_dict["balanced_accuracy_score"]         = SKM.balanced_accuracy_score
-   metric_dict["brier_score_loss"]                = SKM.brier_score_loss
-   metric_dict["classification_report"]           = SKM.classification_report
-   metric_dict["cohen_kappa_score"]               = SKM.cohen_kappa_score
-   metric_dict["confusion_matrix"]                = SKM.confusion_matrix
-   metric_dict["f1_score"]                        = SKM.f1_score
-   metric_dict["fbeta_score"]                     = SKM.fbeta_score
-   metric_dict["hamming_loss"]                    = SKM.hamming_loss
-   metric_dict["hinge_loss"]                      = SKM.hinge_loss
-   metric_dict["log_loss"]                        = SKM.log_loss
-   metric_dict["matthews_corrcoef"]               = SKM.matthews_corrcoef
-   metric_dict["multilabel_confusion_matrix"]     = SKM.multilabel_confusion_matrix
-   metric_dict["precision_recall_curve"]          = SKM.precision_recall_curve
-   metric_dict["precision_recall_fscore_support"] = SKM.precision_recall_fscore_support
-   metric_dict["precision_score"]                 = SKM.precision_score
-   metric_dict["recall_score"]                    = SKM.recall_score
-   metric_dict["roc_auc_score"]                   = SKM.roc_auc_score
-   metric_dict["roc_curve"]                       = SKM.roc_curve
-   metric_dict["jaccard_score"]                   = SKM.jaccard_score
-   metric_dict["zero_one_loss"]                   = SKM.zero_one_loss
-   # regression
-   metric_dict["mean_squared_error"]              = SKM.mean_squared_error
-   metric_dict["mean_squared_log_error"]          = SKM.mean_squared_log_error
-   metric_dict["mean_absolute_error"]             = SKM.mean_absolute_error
-   metric_dict["median_absolute_error"]           = SKM.median_absolute_error
-   metric_dict["r2_score"]                        = SKM.r2_score
-   metric_dict["max_error"]                       = SKM.max_error
-   metric_dict["mean_poisson_deviance"]           = SKM.mean_poisson_deviance
-   metric_dict["mean_gamma_deviance"]             = SKM.mean_gamma_deviance
-   metric_dict["mean_tweedie_deviance"]           = SKM.mean_tweedie_deviance
-   metric_dict["explained_variance_score"]        = SKM.explained_variance_score
+    metric_dict["roc_auc_score"] = SKM.roc_auc_score
+    metric_dict["accuracy_score"] = SKM.accuracy_score
+    metric_dict["auc"] = SKM.auc
+    metric_dict["average_precision_score"] = SKM.average_precision_score
+    metric_dict["balanced_accuracy_score"] = SKM.balanced_accuracy_score
+    metric_dict["brier_score_loss"] = SKM.brier_score_loss
+    metric_dict["classification_report"] = SKM.classification_report
+    metric_dict["cohen_kappa_score"] = SKM.cohen_kappa_score
+    metric_dict["confusion_matrix"] = SKM.confusion_matrix
+    metric_dict["f1_score"] = SKM.f1_score
+    metric_dict["fbeta_score"] = SKM.fbeta_score
+    metric_dict["hamming_loss"] = SKM.hamming_loss
+    metric_dict["hinge_loss"] = SKM.hinge_loss
+    metric_dict["log_loss"] = SKM.log_loss
+    metric_dict["matthews_corrcoef"] = SKM.matthews_corrcoef
+    metric_dict["multilabel_confusion_matrix"] = SKM.multilabel_confusion_matrix
+    metric_dict["precision_recall_curve"] = SKM.precision_recall_curve
+    metric_dict["precision_recall_fscore_support"] = SKM.precision_recall_fscore_support
+    metric_dict["precision_score"] = SKM.precision_score
+    metric_dict["recall_score"] = SKM.recall_score
+    metric_dict["roc_auc_score"] = SKM.roc_auc_score
+    metric_dict["roc_curve"] = SKM.roc_curve
+    metric_dict["jaccard_score"] = SKM.jaccard_score
+    metric_dict["zero_one_loss"] = SKM.zero_one_loss
+    # regression
+    metric_dict["mean_squared_error"] = SKM.mean_squared_error
+    metric_dict["mean_squared_log_error"] = SKM.mean_squared_log_error
+    metric_dict["mean_absolute_error"] = SKM.mean_absolute_error
+    metric_dict["median_absolute_error"] = SKM.median_absolute_error
+    metric_dict["r2_score"] = SKM.r2_score
+    metric_dict["max_error"] = SKM.max_error
+    metric_dict["mean_poisson_deviance"] = SKM.mean_poisson_deviance
+    metric_dict["mean_gamma_deviance"] = SKM.mean_gamma_deviance
+    metric_dict["mean_tweedie_deviance"] = SKM.mean_tweedie_deviance
+    metric_dict["explained_variance_score"] = SKM.explained_variance_score
 end
 
 function checkfun(sfunc::String)
     if !(sfunc in keys(metric_dict))
         println("$sfunc metric is not supported")
-        println("metric: ",keys(metric_dict))
+        println("metric: ", keys(metric_dict))
         error("Metric keyword error")
     end
 end
@@ -91,42 +91,42 @@ and the following metrics for regression:
 - "max_error"
 - "explained_variance_score"
 """
-function crossvalidate(pl::Machine,X::DataFrame,Y::Vector,
-                       sfunc::String; nfolds=10,verbose::Bool=true)
+function crossvalidate(pl::Machine, X::DataFrame, Y::Vector,
+    sfunc::String; nfolds=10, verbose::Bool=true)
     with_span("crossvalidate $(pl.name) $sfunc") do
-        YC=Y
+        YC = Y
         if !(eltype(YC) <: Real)
             YC = Y |> Vector{String}
         end
 
         checkfun(sfunc)
         pfunc = metric_dict[sfunc]
-        metric(a,b) = pfunc(a,b) |> (x -> PYC.pyconvert(Float64,x))
-        res=crossvalidate(pl,X,YC,metric,nfolds,verbose)
+        metric(a, b) = pfunc(a, b) |> (x -> PYC.pyconvert(Float64, x))
+        res = crossvalidate(pl, X, YC, metric, nfolds, verbose)
         return res
     end
 end
 
-function crossvalidate(pl::Machine,X::DataFrame,Y::Vector,sfunc::String,nfolds::Int)
-   crossvalidate(pl,X,Y,sfunc; nfolds)
+function crossvalidate(pl::Machine, X::DataFrame, Y::Vector, sfunc::String, nfolds::Int)
+    crossvalidate(pl, X, Y, sfunc; nfolds)
 end
 
-function crossvalidate(pl::Machine,X::DataFrame,Y::Vector,sfunc::String,verbose::Bool)
-   crossvalidate(pl,X,Y,sfunc; verbose)
+function crossvalidate(pl::Machine, X::DataFrame, Y::Vector, sfunc::String, verbose::Bool)
+    crossvalidate(pl, X, Y, sfunc; verbose)
 end
 
-function crossvalidate(pl::Machine,X::DataFrame,Y::Vector,
-                       sfunc::String, nfolds::Int,verbose::Bool)
-   crossvalidate(pl,X,Y,sfunc; nfolds,verbose)
+function crossvalidate(pl::Machine, X::DataFrame, Y::Vector,
+    sfunc::String, nfolds::Int, verbose::Bool)
+    crossvalidate(pl, X, Y, sfunc; nfolds, verbose)
 end
 
-function crossvalidate(pl::Machine,X::DataFrame,Y::Vector,
-                       sfunc::String,averagetype::String;nfolds=10,verbose::Bool=true)
+function crossvalidate(pl::Machine, X::DataFrame, Y::Vector,
+    sfunc::String, averagetype::String; nfolds=10, verbose::Bool=true)
     with_span("crossvalidate $(pl.name) $sfunc") do
         checkfun(sfunc)
         pfunc = metric_dict[sfunc]
-        metric(a,b) = pfunc(a,b,average=averagetype) |> (x -> PYC.pyconvert(Float64,x))
-        res=crossvalidate(pl,X,Y,metric,nfolds,verbose)
+        metric(a, b) = pfunc(a, b, average=averagetype) |> (x -> PYC.pyconvert(Float64, x))
+        res = crossvalidate(pl, X, Y, metric, nfolds, verbose)
         return res
     end
 end
--- src/skpreprocessor.jl
+++ src/skpreprocessor.jl
@@ -1,7 +1,7 @@
 module SKPreprocessors
 
 import PythonCall
-const PYC=PythonCall
+const PYC = PythonCall
 
 # standard included modules
 using DataFrames
@@ -18,83 +18,83 @@ export fit, fit!, transform, transform!
 export SKPreprocessor, skpreprocessors
 
 const preprocessor_dict = Dict{String,PYC.Py}()
-const DEC  = PYC.pynew()
-const FS   = PYC.pynew()
-const IMP  = PYC.pynew()
+const DEC = PYC.pynew()
+const FS = PYC.pynew()
+const IMP = PYC.pynew()
 const PREP = PYC.pynew()
 
 
 function __init__()
-   PYC.pycopy!(DEC , PYC.pyimport("sklearn.decomposition"))
-   PYC.pycopy!(FS  , PYC.pyimport("sklearn.feature_selection",))
-   PYC.pycopy!(IMP , PYC.pyimport("sklearn.impute"))
-   PYC.pycopy!(PREP, PYC.pyimport("sklearn.preprocessing"))
+    PYC.pycopy!(DEC, PYC.pyimport("sklearn.decomposition"))
+    PYC.pycopy!(FS, PYC.pyimport("sklearn.feature_selection",))
+    PYC.pycopy!(IMP, PYC.pyimport("sklearn.impute"))
+    PYC.pycopy!(PREP, PYC.pyimport("sklearn.preprocessing"))
 
-   # Available scikit-learn learners.
-   preprocessor_dict["DictionaryLearning"]          = DEC
-   preprocessor_dict["FactorAnalysis"]              = DEC
-   preprocessor_dict["FastICA"]                     = DEC
-   preprocessor_dict["IncrementalPCA"]              = DEC
-   preprocessor_dict["KernelPCA"]                   = DEC
-   preprocessor_dict["LatentDirichletAllocation"]   = DEC
-   preprocessor_dict["MiniBatchDictionaryLearning"] = DEC
-   preprocessor_dict["MiniBatchSparsePCA"]          = DEC
-   preprocessor_dict["NMF"]                         = DEC
-   preprocessor_dict["PCA"]                         = DEC
-   preprocessor_dict["SparsePCA"]                   = DEC
-   preprocessor_dict["SparseCoder"]                 = DEC
-   preprocessor_dict["TruncatedSVD"]                = DEC
-   preprocessor_dict["dict_learning"]               = DEC
-   preprocessor_dict["dict_learning_online"]        = DEC
-   preprocessor_dict["fastica"]                     = DEC
-   preprocessor_dict["non_negative_factorization"]  = DEC
-   preprocessor_dict["sparse_encode"]               = DEC
-   preprocessor_dict["GenericUnivariateSelect"]     = FS
-   preprocessor_dict["SelectPercentile"]            = FS
-   preprocessor_dict["SelectKBest"]                 = FS
-   preprocessor_dict["SelectFpr"]                   = FS
-   preprocessor_dict["SelectFdr"]                   = FS
-   preprocessor_dict["SelectFromModel"]             = FS
-   preprocessor_dict["SelectFwe"]                   = FS
-   preprocessor_dict["RFE"]                         = FS
-   preprocessor_dict["RFECV"]                       = FS
-   preprocessor_dict["VarianceThreshold"]           = FS
-   preprocessor_dict["chi2"]                        = FS
-   preprocessor_dict["f_classif"]                   = FS
-   preprocessor_dict["f_regression"]                = FS
-   preprocessor_dict["mutual_info_classif"]         = FS
-   preprocessor_dict["mutual_info_regression"]      = FS
-   preprocessor_dict["SimpleImputer"]               = IMP
-   preprocessor_dict["MissingIndicator"]            = IMP
-   preprocessor_dict["Binarizer"]                   = PREP
-   preprocessor_dict["FunctionTransformer"]         = PREP
-   preprocessor_dict["KBinsDiscretizer"]            = PREP
-   preprocessor_dict["KernelCenterer"]              = PREP
-   preprocessor_dict["LabelBinarizer"]              = PREP
-   preprocessor_dict["LabelEncoder"]                = PREP
-   preprocessor_dict["MultiLabelBinarizer"]         = PREP
-   preprocessor_dict["MaxAbsScaler"]                = PREP
-   preprocessor_dict["MinMaxScaler"]                = PREP
-   preprocessor_dict["Normalizer"]                  = PREP
-   preprocessor_dict["OneHotEncoder"]               = PREP
-   preprocessor_dict["OrdinalEncoder"]              = PREP
-   preprocessor_dict["PolynomialFeatures"]          = PREP
-   preprocessor_dict["PowerTransformer"]            = PREP
-   preprocessor_dict["QuantileTransformer"]         = PREP
-   preprocessor_dict["RobustScaler"]                = PREP
-   preprocessor_dict["StandardScaler"]              = PREP
-   #"IterativeImputer" => IMP.IterativeImputer,
-   #"KNNImputer" => IMP.KNNImputer,
-   #"add_dummy_feature" => PREP.add_dummy_feature,
-   #"binarize" => PREP.binarize,
-   #"label_binarize" => PREP.label_binarize,
-   #"maxabs_scale" => PREP.maxabs_scale,
-   #"minmax_scale" => PREP.minmax_scale,
-   #"normalize" => PREP.normalize,
-   #"quantile_transform" => PREP.quantile_transform,
-   #"robust_scale" => PREP.robust_scale,
-   #"scale" => PREP.scale,
-   #"power_transform" => PREP.power_transform
+    # Available scikit-learn learners.
+    preprocessor_dict["DictionaryLearning"] = DEC
+    preprocessor_dict["FactorAnalysis"] = DEC
+    preprocessor_dict["FastICA"] = DEC
+    preprocessor_dict["IncrementalPCA"] = DEC
+    preprocessor_dict["KernelPCA"] = DEC
+    preprocessor_dict["LatentDirichletAllocation"] = DEC
+    preprocessor_dict["MiniBatchDictionaryLearning"] = DEC
+    preprocessor_dict["MiniBatchSparsePCA"] = DEC
+    preprocessor_dict["NMF"] = DEC
+    preprocessor_dict["PCA"] = DEC
+    preprocessor_dict["SparsePCA"] = DEC
+    preprocessor_dict["SparseCoder"] = DEC
+    preprocessor_dict["TruncatedSVD"] = DEC
+    preprocessor_dict["dict_learning"] = DEC
+    preprocessor_dict["dict_learning_online"] = DEC
+    preprocessor_dict["fastica"] = DEC
+    preprocessor_dict["non_negative_factorization"] = DEC
+    preprocessor_dict["sparse_encode"] = DEC
+    preprocessor_dict["GenericUnivariateSelect"] = FS
+    preprocessor_dict["SelectPercentile"] = FS
+    preprocessor_dict["SelectKBest"] = FS
+    preprocessor_dict["SelectFpr"] = FS
+    preprocessor_dict["SelectFdr"] = FS
+    preprocessor_dict["SelectFromModel"] = FS
+    preprocessor_dict["SelectFwe"] = FS
+    preprocessor_dict["RFE"] = FS
+    preprocessor_dict["RFECV"] = FS
+    preprocessor_dict["VarianceThreshold"] = FS
+    preprocessor_dict["chi2"] = FS
+    preprocessor_dict["f_classif"] = FS
+    preprocessor_dict["f_regression"] = FS
+    preprocessor_dict["mutual_info_classif"] = FS
+    preprocessor_dict["mutual_info_regression"] = FS
+    preprocessor_dict["SimpleImputer"] = IMP
+    preprocessor_dict["MissingIndicator"] = IMP
+    preprocessor_dict["Binarizer"] = PREP
+    preprocessor_dict["FunctionTransformer"] = PREP
+    preprocessor_dict["KBinsDiscretizer"] = PREP
+    preprocessor_dict["KernelCenterer"] = PREP
+    preprocessor_dict["LabelBinarizer"] = PREP
+    preprocessor_dict["LabelEncoder"] = PREP
+    preprocessor_dict["MultiLabelBinarizer"] = PREP
+    preprocessor_dict["MaxAbsScaler"] = PREP
+    preprocessor_dict["MinMaxScaler"] = PREP
+    preprocessor_dict["Normalizer"] = PREP
+    preprocessor_dict["OneHotEncoder"] = PREP
+    preprocessor_dict["OrdinalEncoder"] = PREP
+    preprocessor_dict["PolynomialFeatures"] = PREP
+    preprocessor_dict["PowerTransformer"] = PREP
+    preprocessor_dict["QuantileTransformer"] = PREP
+    preprocessor_dict["RobustScaler"] = PREP
+    preprocessor_dict["StandardScaler"] = PREP
+    #"IterativeImputer" => IMP.IterativeImputer,
+    #"KNNImputer" => IMP.KNNImputer,
+    #"add_dummy_feature" => PREP.add_dummy_feature,
+    #"binarize" => PREP.binarize,
+    #"label_binarize" => PREP.label_binarize,
+    #"maxabs_scale" => PREP.maxabs_scale,
+    #"minmax_scale" => PREP.minmax_scale,
+    #"normalize" => PREP.normalize,
+    #"quantile_transform" => PREP.quantile_transform,
+    #"robust_scale" => PREP.robust_scale,
+    #"scale" => PREP.scale,
+    #"power_transform" => PREP.power_transform
 end
 
 """
@@ -108,56 +108,56 @@ documentation for arguments to pass.
 Implements `fit!` and `transform!`.
 """
 mutable struct SKPreprocessor <: Transformer
-   name::String
-   model::Dict{Symbol,Any}
+    name::String
+    model::Dict{Symbol,Any}
 
-   function SKPreprocessor(args=Dict())
-      default_args=Dict(
-         :name => "skprep",
-         :preprocessor => "PCA",
-         :autocomponent=>false,
-         :impl_args => Dict()
-      )
-      cargs = nested_dict_merge(default_args, args)
-      cargs[:name] = cargs[:name]*"_"*randstring(3)
-      prep = cargs[:preprocessor]
-      if !(prep in keys(preprocessor_dict)) 
-         println("$prep is not supported.") 
-         println()
-         skpreprocessors()
-         error("Argument keyword error")
-      end
-      new(cargs[:name],cargs)
-   end
+    function SKPreprocessor(args=Dict())
+        default_args = Dict(
+            :name => "skprep",
+            :preprocessor => "PCA",
+            :autocomponent => false,
+            :impl_args => Dict()
+        )
+        cargs = nested_dict_merge(default_args, args)
+        cargs[:name] = cargs[:name] * "_" * randstring(3)
+        prep = cargs[:preprocessor]
+        if !(prep in keys(preprocessor_dict))
+            println("$prep is not supported.")
+            println()
+            skpreprocessors()
+            error("Argument keyword error")
+        end
+        new(cargs[:name], cargs)
+    end
 end
 
-function SKPreprocessor(prep::String,args::Dict)
-  SKPreprocessor(Dict(:preprocessor => prep,:name=>prep,args...))
+function SKPreprocessor(prep::String, args::Dict)
+    SKPreprocessor(Dict(:preprocessor => prep, :name => prep, args...))
 end
 
 function SKPreprocessor(prep::String; args...)
-   SKPreprocessor(Dict(:preprocessor => prep,:name=>prep,:impl_args=>Dict(pairs(args))))
+    SKPreprocessor(Dict(:preprocessor => prep, :name => prep, :impl_args => Dict(pairs(args))))
 end
 
-function (skp::SKPreprocessor)(;objargs...)
-   skp.model[:impl_args] = Dict(pairs(objargs))
-   prepname = skp.model[:preprocessor]
-   skobj = getproperty(preprocessor_dict[prepname],prepname)
-   newskobj = skobj(;objargs...)
-   skp.model[:skpreprocessor] = newskobj
-   return skp
+function (skp::SKPreprocessor)(; objargs...)
+    skp.model[:impl_args] = Dict(pairs(objargs))
+    prepname = skp.model[:preprocessor]
+    skobj = getproperty(preprocessor_dict[prepname], prepname)
+    newskobj = skobj(; objargs...)
+    skp.model[:skpreprocessor] = newskobj
+    return skp
 end
 
 function skpreprocessors()
-  processors = keys(preprocessor_dict) |> collect |> x-> sort(x,lt=(x,y)->lowercase(x)<lowercase(y))
-  println("syntax: SKPreprocessor(name::String, args::Dict=Dict())")
-  println("where *name* can be one of:")
-  println()
-  [print(processor," ") for processor in processors]
-  println()
-  println()
-  println("and *args* are the corresponding preprocessor's initial parameters.")
-  println("Note: Please consult Scikitlearn's online help for more details about the preprocessor's arguments.")
+    processors = keys(preprocessor_dict) |> collect |> x -> sort(x, lt=(x, y) -> lowercase(x) < lowercase(y))
+    println("syntax: SKPreprocessor(name::String, args::Dict=Dict())")
+    println("where *name* can be one of:")
+    println()
+    [print(processor, " ") for processor in processors]
+    println()
+    println()
+    println("and *args* are the corresponding preprocessor's initial parameters.")
+    println("Note: Please consult Scikitlearn's online help for more details about the preprocessor's arguments.")
 end
 
 function fit!(skp::SKPreprocessor, x::DataFrame, yc::Vector=[])::Nothing
@@ -174,15 +174,15 @@ function fit!(skp::SKPreprocessor, x::DataFrame, yc::Vector=[])::Nothing
             cols = ncol(x)
             ncomponents = 1
             if cols > 0
-                ncomponents = round(sqrt(cols),digits=0) |> Integer
-                push!(impl_args,:n_components => ncomponents)
+                ncomponents = round(sqrt(cols), digits=0) |> Integer
+                push!(impl_args, :n_components => ncomponents)
             end
         end
         preprocessor = skp.model[:preprocessor]
-        py_preprocessor = getproperty(preprocessor_dict[preprocessor],preprocessor)
+        py_preprocessor = getproperty(preprocessor_dict[preprocessor], preprocessor)
 
         # Train model
-        preproc = py_preprocessor(;impl_args...)
+        preproc = py_preprocessor(; impl_args...)
         preproc.fit(features)
         skp.model[:skpreprocessor] = preproc
         skp.model[:impl_args] = impl_args
@@ -191,21 +191,21 @@ function fit!(skp::SKPreprocessor, x::DataFrame, yc::Vector=[])::Nothing
 end
 
 function fit(skp::SKPreprocessor, x::DataFrame, y::Vector=[])::SKPreprocessor
-   fit!(skp,x,y)
-   return deepcopy(skp)
+    fit!(skp, x, y)
+    return deepcopy(skp)
 end
 
 function transform!(skp::SKPreprocessor, x::DataFrame)::DataFrame
     with_span("transform $(skp.model[:preprocessor])") do
         features = deepcopy(x) |> Array
-        model=skp.model[:skpreprocessor]
+        model = skp.model[:skpreprocessor]
         res = (model.transform(features))
-        myres=PYC.pyconvert(Matrix,res) |> x->DataFrame(x,:auto)
+        myres = PYC.pyconvert(Matrix, res) |> x -> DataFrame(x, :auto)
         return myres
     end
 end
 
-transform(skp::SKPreprocessor, x::DataFrame)::DataFrame = transform!(skp,x)
+transform(skp::SKPreprocessor, x::DataFrame)::DataFrame = transform!(skp, x)
 
 end
 
