329b68f555a4d7893a737cf043c2ca8e3f6ac1a5
Paulito Palmes, PhD <ppalmes@gmail.com>
2024-11-04 19:50:35 +0000

    remove e2eai

--- e2eai/sysimage.jl
+++
@@ -1,7 +0,0 @@
-using PackageCompiler
-create_sysimage(["AMLPipelineBase","DataFrames","StatsBase",
-                  "CSV","Dates","Distributed","TSML",
-                  "Random","ArgParse","Test",
-                  "Statistics","Serialization"], 
-                  sysimage_path="amlp.so", precompile_execution_file="precompile.jl")
-
