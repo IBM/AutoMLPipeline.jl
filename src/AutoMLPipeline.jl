91f45677ec639846150046831e826ac2c5b9b191
Paulito Palmes, PhD <ppalmes@gmail.com>
2025-02-11 14:39:22 +0000

    sample commit

--- src/AutoMLPipeline.jl
+++ src/AutoMLPipeline.jl
@@ -2,7 +2,7 @@ module AutoMLPipeline
 
 using AMLPipelineBase
 using AMLPipelineBase.AbsTypes
-export fit, fit!, transform, transform!,fit_transform, fit_transform!
+export fit, fit!, transform, transform!, fit_transform, fit_transform!
 
 using AMLPipelineBase
 using AMLPipelineBase: AbsTypes, Utils, BaselineModels, Pipelines
@@ -11,18 +11,18 @@ using AMLPipelineBase: EnsembleMethods, CrossValidators
 using AMLPipelineBase: NARemovers
 
 export Machine, Learner, Transformer, Workflow, Computer
-export holdout, kfold, score, infer_eltype, nested_dict_to_tuples, 
-       nested_dict_set!, nested_dict_merge, create_transformer,
-       mergedict, getiris, getprofb,
-       skipmean,skipmedian,skipstd,
-       aggregatorclskipmissing,
-       find_catnum_columns,
-       train_test_split
+export holdout, kfold, score, infer_eltype, nested_dict_to_tuples,
+    nested_dict_set!, nested_dict_merge, create_transformer,
+    mergedict, getiris, getprofb,
+    skipmean, skipmedian, skipstd,
+    aggregatorclskipmissing,
+    find_catnum_columns,
+    train_test_split
 
 
 export Baseline, Identity
-export Imputer,OneHotEncoder,Wrapper
-export PrunedTree,RandomForest,Adaboost
+export Imputer, OneHotEncoder, Wrapper
+export PrunedTree, RandomForest, Adaboost
 export VoteEnsemble, StackEnsemble, BestLearner
 export FeatureSelector, CatFeatureSelector, NumFeatureSelector, CatNumDiscriminator
 export crossvalidate
@@ -50,24 +50,24 @@ export crossvalidate
 export skoperator
 
 function skoperator(name::String; args...)::Machine
-   sklr = keys(SKLearners.learner_dict)
-   skpr = keys(SKPreprocessors.preprocessor_dict)
-   if name ∈ sklr
-      obj = SKLearner(name; args...)
-   elseif name ∈ skpr
-      obj = SKPreprocessor(name; args...)
-   else
-      skoperator()
-      throw(ArgumentError("$name does not exist"))
-   end
-   return obj
+    sklr = keys(SKLearners.learner_dict)
+    skpr = keys(SKPreprocessors.preprocessor_dict)
+    if name ∈ sklr
+        obj = SKLearner(name; args...)
+    elseif name ∈ skpr
+        obj = SKPreprocessor(name; args...)
+    else
+        skoperator()
+        throw(ArgumentError("$name does not exist"))
+    end
+    return obj
 end
 
 function skoperator()
-   sklr = keys(SKLearners.learner_dict)
-   skpr = keys(SKPreprocessors.preprocessor_dict)
-   println("Please choose among these pipeline elements:")
-   println([sklr..., skpr...])
+    sklr = keys(SKLearners.learner_dict)
+    skpr = keys(SKPreprocessors.preprocessor_dict)
+    println("Please choose among these pipeline elements:")
+    println([sklr..., skpr...])
 end
 
 end # module
