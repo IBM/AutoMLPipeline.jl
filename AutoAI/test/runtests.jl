e6cf5aa1c22592d98adb7a8980dfd07b864e6ec5
Paulito Palmes, PhD <ppalmes@gmail.com>
2025-08-22 02:40:29 -0500

    disable mlflow unit-testing

--- AutoAI/test/runtests.jl
+++ AutoAI/test/runtests.jl
@@ -8,15 +8,15 @@ nprocs() == 1 && addprocs()
 
 include("./test_automl.jl")
 
-const tmpdir = tempdir()
-const mlflowpath = joinpath(CondaPkg.envdir(), "bin", "mlflow")
-const backendpath = joinpath("sqlite:///", tmpdir, "mlflow.db")
-const artifactpath = joinpath(tmpdir, "mlruns")
-cmd = "$mlflowpath server --host 127.0.0.1 --port 8080 --backend-store-uri $backendpath --default-artifact-root $artifactpath "
-mlflowprocess = run(Cmd(`sh -c "$cmd"`), wait=false)
-
-include("./test_automlflow.jl")
-
-kill(mlflowprocess)
+#const tmpdir = tempdir()
+#const mlflowpath = joinpath(CondaPkg.envdir(), "bin", "mlflow")
+#const backendpath = joinpath("sqlite:///", tmpdir, "mlflow.db")
+#const artifactpath = joinpath(tmpdir, "mlruns")
+#cmd = "$mlflowpath server --host 127.0.0.1 --port 8080 --backend-store-uri $backendpath --default-artifact-root $artifactpath "
+#mlflowprocess = run(Cmd(`sh -c "$cmd"`), wait=false)
+#
+#include("./test_automlflow.jl")
+#
+#kill(mlflowprocess)
 
 end
