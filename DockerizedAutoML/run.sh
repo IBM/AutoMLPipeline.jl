abb965edd648a93559df83fbd3ca85c556f646d6
Paulito Palmes, PhD <ppalmes@gmail.com>
2025-08-22 17:36:59 +0100

    minor edit of main.jl
    Signed-off-by: Paulito Palmes, PhD <ppalmes@gmail.com>

--- DockerizedAutoML/run.sh
+++ DockerizedAutoML/run.sh
@@ -5,4 +5,5 @@ docker run -it --rm --platform=linux/amd64 automlai
 # julia --project -- ./main.jl -c low -t classification -f 3 -w 3 iris.csv
 # julia --project -- ./main.jl -c low -t anomalydetection iris.csv
 # podman run -it --rm --platform=linux/amd64 localhost/automlai -u http://spendor2.sl.cloud9.ibm.com:30412 iris.csv
+# podman run -it --rm -v `pwd`:/data/  localhost/automlai -u http://spendor2.sl.cloud9.ibm.com:30412 -t regression /data/iris_reg.csv
 # julia --project -- ./main.jl -c low -t classification -f 3 -w 3 iris.csv --predict_only --runid cd4e463d6a414aa4aaad173e567d7d22 -o /tmp/hello.txt
