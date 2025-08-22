76a19881804f15392e2392f860a64051364fb10c
Paulito Palmes, PhD <ppalmes@gmail.com>
2025-08-22 11:01:02 +0100

    adding argo templates and autoad dockerization

---
+++ DockerizedAutoAD/automl_precompile.jl
@@ -0,0 +1,8 @@
+import AMLPipelineBase
+include(joinpath(pkgdir(AMLPipelineBase), "test", "runtests.jl"))
+
+import AutoMLPipeline
+include(joinpath(pkgdir(AutoMLPipeline), "test", "runtests.jl"))
+
+import AutoAI
+include(joinpath(pkgdir(AutoAI), "test", "runtests.jl"))
