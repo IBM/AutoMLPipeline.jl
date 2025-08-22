266361bce5b65e6bb6ef2b5781d93190c1b00bd3
Paulito Palmes, PhD <ppalmes@gmail.com>
2025-08-22 11:01:02 +0100

    adding argo templates and autoad dockerization

---
+++ DockerizedAutoAD/run.sh
@@ -0,0 +1,7 @@
+docker build -t automlai --platform=linux/amd64 .
+docker run -it --rm --platform=linux/amd64 automlai
+
+# julia --project -- ./main.jl -c high -t regression -f 3 -w 7 iris_reg.csv
+# julia --project -- ./main.jl -c low -t classification -f 3 -w 3 iris.csv
+# julia --project -- ./main.jl -c low -t anomalydetection iris.csv
+# podman run -it --rm --platform=linux/amd64 localhost/automlai -u http://spendor2.sl.cloud9.ibm.com:30412 iris.csv
