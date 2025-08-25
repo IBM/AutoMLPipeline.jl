123da1eb48e3903d1032b39ba43082ce027c70eb
Paulito Palmes, PhD <ppalmes@gmail.com>
2025-08-25 15:34:03 +0100

    minor edit of telemetry extraction
    Signed-off-by: Paulito Palmes, PhD <ppalmes@gmail.com>

--- AutoAD/data/telemetry/run.sh
+++ AutoAD/data/telemetry/run.sh
@@ -1,1 +1,3 @@
-curl 'http://localhost:9090/api/v1/query?query={cluster:node_cpu:ratio_rate5m}\[1d:1m\]' | jq '.data.result[].values[][1]' | sed 's/\"//g' >node_cpu_ratio_rate_5m_1d_1m.csv
+curl 'http://localhost:9090/api/v1/query?query={cluster:node_cpu:ratio_rate5m}\[1d:1m\]' |
+	jq '.data.result[].values[][1]' |
+	sed 's/\"//g' >node_cpu_ratio_rate_5m_1d_1m.csv
