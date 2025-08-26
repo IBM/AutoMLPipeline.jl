18ce67575a628d1fca96e42e42e3f9a49c1c4e8b
Paulito Palmes, PhD <ppalmes@gmail.com>
2025-08-26 17:48:45 -0500

    bug fixes

--- DockerizedAutoAD/run.sh
+++ DockerizedAutoAD/run.sh
@@ -5,3 +5,4 @@ docker run -it --rm --platform=linux/amd64 automlad
 # julia --project -- ./main.jl -c low -t classification -f 3 -w 3 iris.csv
 # julia --project -- ./main.jl -c low -t anomalydetection iris.csv
 # podman run -it --rm --platform=linux/amd64 localhost/automlai -u http://spendor2.sl.cloud9.ibm.com:30412 iris.csv
+podman run -it --rm --platform=linux/amd64 localhost/automlad -u http://spendor3.sl.cloud9.ibm.com:30412 ../AutoAD/data/node_cpu_ratio_rate_5m_1d_1m.csv
