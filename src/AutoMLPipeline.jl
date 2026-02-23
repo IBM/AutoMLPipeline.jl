89865bbcc87e39c11d4fc0e02a1a2a6b0cdd1e07
Paulito Palmes, PhD <ppalmes@gmail.com>
2026-02-23 21:31:30 +0000

    lsp reformat

--- src/AutoMLPipeline.jl
+++ src/AutoMLPipeline.jl
@@ -12,12 +12,12 @@ using AMLPipelineBase: NARemovers
 
 export Machine, Learner, Transformer, Workflow, Computer
 export holdout, kfold, score, infer_eltype, nested_dict_to_tuples,
-    nested_dict_set!, nested_dict_merge, create_transformer,
-    mergedict, getiris, getprofb,
-    skipmean, skipmedian, skipstd,
-    aggregatorclskipmissing,
-    find_catnum_columns,
-    train_test_split
+  nested_dict_set!, nested_dict_merge,
+  mergedict, getiris, getprofb,
+  skipmean, skipmedian, skipstd,
+  aggregatorclskipmissing,
+  find_catnum_columns,
+  train_test_split
 
 
 export Baseline, Identity
@@ -50,24 +50,24 @@ export crossvalidate
 export skoperator
 
 function skoperator(name::String; args...)::Machine
-    sklr = keys(SKLearners.learner_dict)
-    skpr = keys(SKPreprocessors.preprocessor_dict)
-    if name ∈ sklr
-        obj = SKLearner(name; args...)
-    elseif name ∈ skpr
-        obj = SKPreprocessor(name; args...)
-    else
-        skoperator()
-        throw(ArgumentError("$name does not exist"))
-    end
-    return obj
+  sklr = keys(SKLearners.learner_dict)
+  skpr = keys(SKPreprocessors.preprocessor_dict)
+  if name ∈ sklr
+    obj = SKLearner(name; args...)
+  elseif name ∈ skpr
+    obj = SKPreprocessor(name; args...)
+  else
+    skoperator()
+    throw(ArgumentError("$name does not exist"))
+  end
+  return obj
 end
 
 function skoperator()
-    sklr = keys(SKLearners.learner_dict)
-    skpr = keys(SKPreprocessors.preprocessor_dict)
-    println("Please choose among these pipeline elements:")
-    println([sklr..., skpr...])
+  sklr = keys(SKLearners.learner_dict)
+  skpr = keys(SKPreprocessors.preprocessor_dict)
+  println("Please choose among these pipeline elements:")
+  println([sklr..., skpr...])
 end
 
 end # module
